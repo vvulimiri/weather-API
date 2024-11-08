6c88e129c2598b419ff4a372535500e89383689f
vvulimiri <86018431+vvulimiri@users.noreply.github.com>
2024-11-08 06:44:37 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI =  'sqlite:///C:/Users/sunil/ManasaPython/WeatherAPI/WeatherDB.db'
+    SQLALCHEMY_DATABASE_URI =  'sqlite:///C:/Users/manasa/WeatherAPI/WeatherDB.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SWAGGER_UI_DOC_EXPANSION = 'none'
     SWAGGER_UI_JSONEDITOR = True
